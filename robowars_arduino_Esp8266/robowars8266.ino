19b5a11a6af5fa8518c4117b336101733796a276
Fatih Sultan Mehmet <1453fatih>
2021-04-23 20:41:01 +0300

    update

---
+++ robowars_arduino_Esp8266/robowars8266.ino
@@ -0,0 +1,288 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#include <Servo.h>
+
+
+#define FIREBASE_HOST " "    
+#define FIREBASE_AUTH " "                    
+#define WIFI_SSID " "               
+#define WIFI_PASSWORD " "             
+
+Servo servo;
+
+FirebaseData fbdo;
+
+FirebaseJson json;
+
+void printResult(FirebaseData &data);
+
+void setup()
+{
+
+  Serial.begin(9600);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  servo.attach(4);
+  servo.write(90);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+
+  fbdo.setBSSLBufferSize(1024, 1024);
+
+  //Set the size of HTTP response buffers in the case where we want to work with large data.
+  fbdo.setResponseSize(1024);
+
+  //Set database read timeout to 1 minute (max 15 minutes)
+  Firebase.setReadTimeout(fbdo, 1000 * 60);
+  //tiny, small, medium, large and unlimited.
+  //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
+  Firebase.setwriteSizeLimit(fbdo, "tiny");
+
+  //optional, set the decimal places for float and double data to be stored in database
+  Firebase.setFloatDigits(2);
+  Firebase.setDoubleDigits(6);
+
+  /*
+  This option allows get and delete functions (PUT and DELETE HTTP requests) works for device connected behind the
+  Firewall that allows only GET and POST requests.
+  
+  Firebase.enableClassicRequest(fbdo, true);
+  */
+
+  String path = "/robowars_Fatih"; //data way
+
+  Serial.println("------------------------------------");
+// Firebase.getString( fbdo,path + "/right") ;
+ //String testss= fbdo.stringData();
+ //if(testss == "test")
+//{Serial.println("oldu bu iş");
+//servo.write(80);
+//}
+
+
+
+/*
+ Serial.println("------------------------------------");
+
+  for (uint8_t i = 0; i < 10; i++)
+  {
+    //Also can use Firebase.set instead of Firebase.setDouble
+    if (Firebase.setDouble(fbdo, path + "/Double/Data" + (i + 1), ((i + 1) * 10) + 0.123456789))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+      Serial.println("ETag: " + fbdo.ETag());
+      Serial.print("VALUE: ");
+      printResult(fbdo);
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+  }
+ 
+  Serial.println("------------------------------------");
+  Serial.println("Get double test...");
+
+  for (uint8_t i = 0; i < 10; i++)
+  {
+    //Also can use Firebase.get instead of Firebase.setInt
+    if (Firebase.getInt(fbdo, path + "/Double/Data" + (i + 1)))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+      Serial.println("ETag: " + fbdo.ETag());
+      Serial.print("VALUE: ");
+      printResult(fbdo);
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+  }
+
+ */
+
+
+
+}
+/*
+void printResult(FirebaseData &data)
+{
+
+  if (data.dataType() == "int")
+    Serial.println(data.intData());
+  else if (data.dataType() == "float")
+    Serial.println(data.floatData(), 5);
+  else if (data.dataType() == "double")
+    printf("%.9lf\n", data.doubleData());
+  else if (data.dataType() == "boolean")
+    Serial.println(data.boolData() == 1 ? "true" : "false");
+  else if (data.dataType() == "string")
+    Serial.println(data.stringData());
+  else if (data.dataType() == "json")
+  {
+    Serial.println();
+    FirebaseJson &json = data.jsonObject();
+    //Print all object data
+    Serial.println("Pretty printed JSON data:");
+    String jsonStr;
+    json.toString(jsonStr, true);
+    Serial.println(jsonStr);
+    Serial.println();
+    Serial.println("Iterate JSON data:");
+    Serial.println();
+    size_t len = json.iteratorBegin();
+    String key, value = "";
+    int type = 0;
+    for (size_t i = 0; i < len; i++)
+    {
+      json.iteratorGet(i, type, key, value);
+      Serial.print(i);
+      Serial.print(", ");
+      Serial.print("Type: ");
+      Serial.print(type == FirebaseJson::JSON_OBJECT ? "object" : "array");
+      if (type == FirebaseJson::JSON_OBJECT)
+      {
+        Serial.print(", Key: ");
+        Serial.print(key);
+      }
+      Serial.print(", Value: ");
+      Serial.println(value);
+    }
+    json.iteratorEnd();
+  }
+  else if (data.dataType() == "array")
+  {
+    Serial.println();
+    //get array data from FirebaseData using FirebaseJsonArray object
+    FirebaseJsonArray &arr = data.jsonArray();
+    //Print all array values
+    Serial.println("Pretty printed Array:");
+    String arrStr;
+    arr.toString(arrStr, true);
+    Serial.println(arrStr);
+    Serial.println();
+    Serial.println("Iterate array values:");
+    Serial.println();
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+      Serial.print(i);
+      Serial.print(", Value: ");
+
+      FirebaseJsonData &jsonData = data.jsonData();
+      //Get the result data from FirebaseJsonArray object
+      arr.get(jsonData, i);
+      if (jsonData.typeNum == FirebaseJson::JSON_BOOL)
+        Serial.println(jsonData.boolValue ? "true" : "false");
+      else if (jsonData.typeNum == FirebaseJson::JSON_INT)
+        Serial.println(jsonData.intValue);
+      else if (jsonData.typeNum == FirebaseJson::JSON_FLOAT)
+        Serial.println(jsonData.floatValue);
+      else if (jsonData.typeNum == FirebaseJson::JSON_DOUBLE)
+        printf("%.9lf\n", jsonData.doubleValue);
+      else if (jsonData.typeNum == FirebaseJson::JSON_STRING ||
+               jsonData.typeNum == FirebaseJson::JSON_NULL ||
+               jsonData.typeNum == FirebaseJson::JSON_OBJECT ||
+               jsonData.typeNum == FirebaseJson::JSON_ARRAY)
+        Serial.println(jsonData.stringValue);
+    }
+  }
+  else if (data.dataType() == "blob")
+  {
+
+    Serial.println();
+
+    for (int i = 0; i < data.blobData().size(); i++)
+    {
+      if (i > 0 && i % 16 == 0)
+        Serial.println();
+
+      if (i < 16)
+        Serial.print("0");
+
+      Serial.print(data.blobData()[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+  else if (data.dataType() == "file")
+  {
+
+    Serial.println();
+
+    File file = data.fileStream();
+    int i = 0;
+
+    while (file.available())
+    {
+      if (i > 0 && i % 16 == 0)
+        Serial.println();
+
+      int v = file.read();
+
+      if (v < 16)
+        Serial.print("0");
+
+      Serial.print(v, HEX);
+      Serial.print(" ");
+      i++;
+    }
+    Serial.println();
+    file.close();
+  }
+  else
+  {
+    Serial.println(data.payload());
+  }
+}
+*/
+void loop()
+{
+    String path = "/robowars_Fatih";
+Firebase.getString(fbdo, path+ "/turn/left"); 
+String leftData = fbdo.stringData();
+Serial.println(leftData);
+Firebase.getString(fbdo, path+ "/turn/right");
+String rightData = fbdo.stringData();
+Serial.println(rightData);
+if(leftData=="left"){
+  servo.write(150);
+    Serial.println("motor sola dönderilmiştir");
+  }
+  else if ( leftData == "stop" && rightData=="stop"){
+   servo.write(90);
+    }
+    if(rightData=="right"){
+      Serial.println("motor sağa dönderilmiştir");
+  servo.write(30);
+
+  }
+  else if ( rightData == "stop" && leftData== "stop"){
+   servo.write(90);
+    }
+  
+}
